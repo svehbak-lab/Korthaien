64a7b646c3cd12476c8c994b531873f195aca8cd
svehbak-lab <svehbak@gmail.com>
2026-09-10 05:17:11 +0000

    Hosting av kundeside og admin

--- korthaien-kjop/src/server.ts
+++ korthaien-kjop/src/server.ts
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   }
   if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
